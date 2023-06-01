77f1c40d6ebd988c968cd1109af721f20217f688
MeekoDev <DiegoCru1024@hotmail.com>
2023-06-01 01:21:48 -0500

    Versión 2.5
    Corrección menor de errores

--- cliente/src/components/createAccountPage.js
+++ cliente/src/components/createAccountPage.js
@@ -34,7 +34,6 @@ export default function CreateAccountPage() {
         setData(prevState => ({
             ...prevState, [name]: value
         }))
-        console.log(data)
     }
 
     const enviarDatos = async (e) => {
