1a278d92ca7abdadcd8c429aa5c758f3ab7b557c
MeekoDev <DiegoCru1024@hotmail.com>
2023-06-10 19:36:26 -0500

    Versión 3.6

--- servidor/routes/getLastMovementsRoute.js
+++ servidor/routes/getLastMovementsRoute.js
@@ -1,16 +1,31 @@
 const router = require('express').Router()
+const {accountModel} = require('../models/accountSchema')
 const {movementModel} = require('../models/movementSchema')
 
 router.get('/', async (req, res) => {
     const studentCode = req.query.studentCode;
 
-    movementModel.find({$or: [{accountOriginID: studentCode.toString()}, {accountDetinyID: studentCode.toString()}]})
-        .then(movementModels => {
-            res.json(movementModels);
+    accountModel.find({ownerUserID: studentCode.toString()})
+        .then(accounts => {
+            const accountIDs = accounts.map(account => account.accountID);
+
+            movementModel.find({
+                $or: [
+                    {accountOriginID: {$in: accountIDs}},
+                    {accountDetinyID: {$in: accountIDs}}
+                ]
+            })
+                .then(movements => {
+                    res.json(movements);
+                })
+                .catch(error => {
+                    console.error(error);
+                    res.status(500).json({message: 'Error interno de servidor'});
+                });
         })
         .catch(error => {
             console.error(error);
-            res.status(500).send({message: 'Error interno de servidor...'});
+            res.status(500).json({message: 'Error interno de servidor'});
         });
 })
 
