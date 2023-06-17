3832945460b052dd21008c2749aba64ebc28d72d
MeekoDev <DiegoCru1024@hotmail.com>
2023-06-17 00:31:20 -0500

    Versión 3.9

--- servidor/routes/loanRequestRoute.js
+++ servidor/routes/loanRequestRoute.js
@@ -1,35 +1,33 @@
 const router = require('express').Router();
-const {loanRequestModel} = require('../models/loanRequestSchema');
+const { loanRequestModel } = require('../models/loanRequestSchema');
 
 router.post('/', (req, res) => {
     const studentCode = req.body.studentCode;
     let hasExistingRequest = false;
 
     if (req.body.loanValue < 0 || req.body.loanFeeRate < 0 || req.body.destinyAccountID < 0) {
-        return req.status(400).send({message: 'Complete los datos antes de enviar...'})
+        return res.status(400).send({ message: 'Complete los datos antes de enviar...' });
     }
 
-    loanRequestModel.findOne({studentCode: studentCode.toString()})
+    loanRequestModel.findOne({ studentCode: studentCode.toString() })
         .then((existingRequest) => {
             if (existingRequest) {
                 hasExistingRequest = true;
-                return;
+            } else {
+                return new loanRequestModel(req.body).save();
             }
-
-            return new loanRequestModel(req.body).save();
         })
         .then(() => {
             if (hasExistingRequest) {
-                res.status(400).send({message: 'Ya tienes una solicitud pendiente...'});
+                res.status(400).send({ message: 'Ya tienes una solicitud pendiente...' });
             } else {
-                res.status(200).send({message: 'Solicitud enviada con éxito...'});
+                res.status(200).send({ message: 'Solicitud enviada con éxito...' });
             }
         })
         .catch((error) => {
             console.log(error);
-            res.status(500).send({message: 'Error interno de servidor...'});
+            res.status(500).send({ message: 'Error interno de servidor...' });
         });
 });
-
 
 module.exports = router;
