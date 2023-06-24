237519c626afbcf906ff054963166958ca9d6661
MeekoDev <DiegoCru1024@hotmail.com>
2023-06-23 20:57:20 -0500

    Update

--- cliente/src/config.js
+++ cliente/src/config.js
@@ -1,2 +1,2 @@
-// export const API_URL = 'https://bankapp-backend.onrender.com'
-export const API_URL = 'http://localhost:5000'
+export const API_URL = 'https://bankapp-backend.onrender.com'
+//export const API_URL = 'http://localhost:5000'
