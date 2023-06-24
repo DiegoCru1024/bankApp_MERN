fe0bd029f6a1db9126e31bb8cf0028aa5b7bfbe4
MeekoDev <DiegoCru1024@hotmail.com>
2023-06-23 20:53:13 -0500

    Versión 4.6

--- cliente/src/components/loanInfoPage.js
+++ cliente/src/components/loanInfoPage.js
@@ -4,11 +4,11 @@ import {API_URL} from "../config";
 import axios from "axios";
 
 export default function LoanInfoPage() {
-    // Obtiene los datos almacenados en el localStorage
-    const storedModel = localStorage.getItem('studentData')
-    const parsedModel = JSON.parse(storedModel)
+    const storedModel = localStorage.getItem('studentData');
+    const parsedModel = JSON.parse(storedModel);
 
     const [loanInfo, setLoanInfo] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const getLoanInfo = async () => {
@@ -20,13 +20,15 @@ export default function LoanInfoPage() {
                     }
                 });
                 setLoanInfo(response.data);
+                setLoading(false);
             } catch (error) {
                 console.log(error);
+                setLoading(false);
             }
         };
 
         getLoanInfo().then(() => {
-            console.log('Datos recibidos...')
+            console.log('Datos recibidos...');
         });
     }, [parsedModel.studentCode]);
 
@@ -37,21 +39,29 @@ export default function LoanInfoPage() {
             <section className='create-account-main-container'>
                 <div className='loanInfo-container'>
                     <h1>Información de préstamo</h1>
-                    <div>
-                        <p><span>Fecha de solicitud: </span> {loanInfo.loanSubmitDate}</p>
-                    </div>
-                    <div>
-                        <p><span>Solicitante:</span> {loanInfo.studentName}</p>
-                        <p><span>Código:</span> {loanInfo.studentCode}</p>
-                    </div>
-                    <div>
-                        <p><span>Valor de préstamo: </span> {loanInfo.loanCurrencyType} {loanInfo.loanValue}</p>
-                        <p><span>Número de cuotas: </span> {loanInfo.loanFeeRate}</p>
-                    </div>
-                    <div className='loanJustification'>
-                        <span>Justificación de préstamo:</span>
-                        <p>{loanInfo.loanJustification}</p>
-                    </div>
+                    {loading ? (
+                        <p>Cargando datos...</p>
+                    ) : (
+                        <>
+                            <div>
+                                <p><span>Fecha de solicitud: </span> {loanInfo && loanInfo.loanSubmitDate}</p>
+                            </div>
+                            <div>
+                                <p><span>Solicitante:</span> {loanInfo && loanInfo.studentName}</p>
+                                <p><span>Código:</span> {loanInfo && loanInfo.studentCode}</p>
+                            </div>
+                            <div>
+                                <p>
+                                    <span>Valor de préstamo: </span> {loanInfo && loanInfo.loanCurrencyType} {loanInfo && loanInfo.loanValue}
+                                </p>
+                                <p><span>Número de cuotas: </span> {loanInfo && loanInfo.loanFeeRate}</p>
+                            </div>
+                            <div className='loanJustification'>
+                                <span>Justificación de préstamo:</span>
+                                <p>{loanInfo && loanInfo.loanJustification}</p>
+                            </div>
+                        </>
+                    )}
                 </div>
             </section>
         </main>
--- servidor/routes/loanInfoRoute.js
+++ servidor/routes/loanInfoRoute.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 
 
 router.get('/', (req, res) => {
-    const studentCode = req.query.studentCode.toString();
+    const studentCode = req.query.studentCode;
 
     loanRequestModel.findOne({studentCode: studentCode}, {_id: 0}) // Excluye el campo _id en la respuesta
         .then(loan => {
