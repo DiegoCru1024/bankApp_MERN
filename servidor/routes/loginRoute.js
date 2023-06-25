ab6a5d98268a4582ce0edcfe4d236904718ffbdc
MeekoDev <DiegoCru1024@hotmail.com>
2023-06-25 18:46:28 -0500

    Update loginRoute.js

--- servidor/routes/loginRoute.js
+++ servidor/routes/loginRoute.js
@@ -1,48 +1,54 @@
-const router = require('express').Router()
-const {studentModel} = require('../models/studentSchema')
-const bcrypt = require('bcrypt')
-const joi = require('joi')
+const router = require('express').Router();
+const { studentModel } = require('../models/studentSchema');
+const bcrypt = require('bcrypt');
+const joi = require('joi');
 
-router.post('/', async (req, res) => {
+router.post('/', (req, res) => {
     try {
-        //Comprobamos si los datos ingresados son correctos
-        const {dataError} = validarDatos(req.body)
-        if (dataError) {
-            return res.status(400).send({message: dataError.details[0].message})
+        // Comprobamos si los datos ingresados son correctos
+        const { error } = validarDatos(req.body);
+        if (error) {
+            return res.status(400).send({ message: error.details[0].message });
         }
 
-        studentModel.findOne({email: req.body.email.toString()})
+        studentModel.findOne({ email: req.body.email })
             .then((modelResponse) => {
-                //Comprobamos que el usuario este registrado
+                // Comprobamos que el usuario esté registrado
                 if (!modelResponse) {
-                    return res.status(401).send({message: 'Correo o contraseña inválida...'})
+                    return res.status(401).send({ message: 'Correo o contraseña inválida...' });
                 }
 
-                //Comprobamos las credenciales de acceso
-                bcrypt.compare(req.body.password, modelResponse.password).then((validPassword) => {
-                    if (!validPassword) {
-                        return res.status(401).send({message: 'Correo o contraseña inválida...'})
-                    }
-                })
+                // Comprobamos las credenciales de acceso
+                bcrypt.compare(req.body.password, modelResponse.password)
+                    .then((validPassword) => {
+                        if (!validPassword) {
+                            return res.status(401).send({ message: 'Correo o contraseña inválida...' });
+                        }
 
-                //Generamos el Token de Autenticación
-                const token = studentModel.generarToken
-                res.status(200).send({data: token, model: modelResponse, message: 'Inicio de sesión exitoso...'})
+                        // Generamos el Token de Autenticación
+                        const token = studentModel.generarToken();
+                        res.status(200).send({ data: token, model: modelResponse, message: 'Inicio de sesión exitoso...' });
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                        res.status(500).send({ message: 'Error interno de servidor...' });
+                    });
             })
             .catch((error) => {
-                console.log(error)
-            })
+                console.log(error);
+                res.status(500).send({ message: 'Error interno de servidor...' });
+            });
     } catch (e) {
-        res.status(500).send({message: 'Error interno de servidor...'})
+        res.status(500).send({ message: 'Error interno de servidor...' });
     }
-})
+});
 
-module.exports = router
+module.exports = router;
 
 const validarDatos = (data) => {
-    const Schema = joi.object({
+    const schema = joi.object({
         email: joi.string().email().required().label('Email'),
-        password: joi.string().required().label('Password')
-    })
-    return Schema.validate(data)
-}
+        password: joi.string().required().label('Password'),
+    });
+    return schema.validate(data);
+};
